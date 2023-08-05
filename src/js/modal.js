e899ebe65b67fdb1abcf0c060fa363e5a9daa1c9
Anna <annanovytskat@gmail.com>
2023-08-05 13:08:15 +0100

    Update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,13 +1,13 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn.forEach(openModalBtn => {
+    openModalBtn.addEventListener('click', toggleModal);
+  });
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
   }
